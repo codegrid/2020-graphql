e4882774fd874179056cc500146d9d440deeeaf2
cancer <zodiac.cancer.j6@gmail.com>
2021-04-14 11:43:03 +0900

    refactor

--- 8/src/api.js
+++ 8/src/api.js
@@ -1,4 +1,21 @@
-import { request } from "./request.js";
+export const request = async ({query, variables}) => {
+  const res = await fetch(
+    "https://api-ap-northeast-1.graphcms.com/v2/cki8biu3885qw01z94eogghe9/master",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({query, variables}),
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("failed to fetch.");
+  }
+
+  return res.json();
+};
 
 /* 画像サイズを指定しない場合のコード*/
 export const fetchArticle = async (slug) => {
